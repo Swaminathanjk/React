4f53b61e317b5412a6d1403a26802d035ca52f4a
Swaminathanjk <swaminathanjk@gmail.com>
2024-10-25 20:19:32 +0530

    redux

---
+++ social-media-redux/src/components/Empty_postpage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Empty_postpage = ({ add_api }) => {
+  return (
+    <div>
+      <center>
+        <h1>Profile Empty</h1>
+      </center>
+    </div>
+  );
+};
+
+export default Empty_postpage;
---
+++ social-media-redux/src/components/postpage/Post.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { MdDelete } from "react-icons/md";
+import { PostList } from "../Context/post-context";
+
+const Post = ({ post }) => {
+  const { deletefromList, likePost } = useContext(PostList);
+  return (
+    <div className="card mb-3 " style={{ width: "18rem" }}>
+      <div className="card-body">
+        <span className=" position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger ">
+          <MdDelete onClick={() => deletefromList(post.id)} /> 
+        </span>
+        <h5 className="card-title">{post.title}</h5>
+        <p className="card-text">{post.body}</p>
+        <div className="d-flex flex-wrap mb-3">
+          {post.tags.map((tag, index) => (
+            <span key={index} className="badge bg-primary me-1">
+              {tag}
+            </span>
+          ))}
+        </div>
+        <button className="btn btn-primary" onClick={() => likePost(post.id)}>
+          Like 👍 {post.reactions}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
